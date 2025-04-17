339e8f50245032780e3ac7293fd00170f2f0e98a
souperseth <sethromanenko@gmail.com>
2025-04-17 09:44:44 -0700

    lock file

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -32,11 +32,6 @@ export default function NavBar() {
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex shrink-0 items-center">
-              {/* <img
-                alt="Your Company"
-                src={logo}
-                className="h-8 w-auto"
-              /> */}
               {/* <Logo/> */}
             </div>
             <div className="hidden sm:ml-6 sm:block">
