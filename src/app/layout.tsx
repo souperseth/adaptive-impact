c432f7ada146e29129e4d601c11691a98a6bae7e
souperseth <sethromanenko@gmail.com>
2025-05-06 09:47:12 -0700

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
 import type { Metadata } from "next";
-import styles from "./globals.css";
-import { kumbhSans } from "./ui/fonts";
 import NavBar from "./Navbar";
 import { Analytics } from "@vercel/analytics/react";
 
@@ -27,11 +25,12 @@ export default function RootLayout({
       <head>
         <script async src="https://widgets.givebutter.com/latest.umd.cjs?acct=lWUgvX8EzEFcXOXe&p=other"/>
       </head>
-      <body className={styles.background}>
+      <body>
         <NavBar/>
         {children}
         {/* @ts-ignore */}
         <givebutter-widget id="L4DzEL"></givebutter-widget>
+        <Analytics />
       </body>
     </html>
   );
